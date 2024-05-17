4626c973d0ced48d1a37fd32fa4283c9674711a6
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2024-05-16 20:26:36 -0400

    Support for uploading attachments

--- core/src/grist/Api.scala
+++ core/src/grist/Api.scala
@@ -6,11 +6,11 @@ import io.circe.*
 import zio.http.*
 import zio.{Task, ZIO}
 
-abstract class Api(baseClient: Client) {
+abstract class Api(protected val baseClient: Client) {
   private val printer    = Printer.noSpaces.copy(dropNullValues = true)
   private val jsonClient = baseClient.contramap[Json](json => Body.fromString(printer.print(json)))
 
-  private def parse[R: Decoder](client: ZClient[?, ?, ?, ?])(response: Response): Task[R] =
+  protected def parse[R: Decoder](client: ZClient[?, ?, ?, ?])(response: Response): Task[R] =
     response.body.asString.flatMap { str =>
       ZIO.fromEither(
         jawn
@@ -29,7 +29,7 @@ abstract class Api(baseClient: Client) {
     client.get("").flatMap(parse[R](client))
   }
 
-  protected def post[P: Encoder, R: Decoder](body: P, queryParams: QueryParams = QueryParams.empty): Task[R] =
+  protected def postJson[P: Encoder, R: Decoder](body: P, queryParams: QueryParams = QueryParams.empty): Task[R] =
     ZIO.scoped {
       val client = jsonClient.addQueryParams(queryParams)
       client.post("")(body.asJson).flatMap(parse[R](client))
